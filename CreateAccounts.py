4976d2d86fc6e9309db5a94b5f591c10e27c8611
Georgi <mishgeorgi@gmail.com>
2021-02-27 20:17:05 +0300

    Feat: add prepare to create clients

--- CreateAccounts.py
+++ CreateAccounts.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 
 # ----system settings-----
 driverBrowserExecutablePath = "C:/chromedriver.exe"
+secondsToWaitLoadelementOnPage = 0.1
+secondsToWaitLoadPage = 10
 
 #-site and account data-
 urlRootWebPage="https://klientiks.ru/"
@@ -18,7 +20,7 @@ phoneCountry = "RU" #to select phone countr (pattern of number)
 #> get driver and open sing in page
 driver = webdriver.Chrome(driverBrowserExecutablePath)
 driver.get(urlLoginPage); #Open sing in page
-time.sleep(6) # wait load page
+time.sleep(secondsToWaitLoadPage) # wait load page
 
 #> sing in
 idElementLogin = "label-phone"
@@ -33,18 +35,34 @@ print("Typed phone phone country")
 idElementPassword = "label-password"
 elementPasswordInput = driver.find_element_by_id(idElementPassword)
 elementPasswordInput.send_keys(accountPassword)
-print("Typed password")
+print("Typed password") 
 xpatchElementButtonSubmitSingIn = "/html/body/div[3]/div[2]/div/div[4]/div/div/div[1]/div[1]/div[2]/form/div[5]/div/div[2]/button"
 elementSingInButton = driver.find_element_by_xpath(xpatchElementButtonSubmitSingIn)
-elementSingInButton.click()
-print("Click button sing in")
+elementSingInButton.click() # Click button sing in and sing in
+print("Clicked button sing in")
+time.sleep(secondsToWaitLoadPage) # wait load page
 
-time.sleep(15) #Let see something
+#jump to clients page
+xpatchRefClientsPage = '/html/body/div[3]/div[1]/div[1]/div[5]/a'
+refClientPage = driver.find_element_by_xpath(xpatchRefClientsPage)
+refClientPage.click()
+print("Clicked ref clients page")
+time.sleep(secondsToWaitLoadPage) # wait load page
+handleOfTabOpenedRef =  driver.window_handles[1] # get handle of new tab where opened reference of page client
+driver.switch_to.window(handleOfTabOpenedRef) # open this tab
+
+#open form to create client
+xpatchRefClientsPage = '/html/body/div[3]/div[1]/div[1]/div[1]/div/div[1]/div[4]/div'
+refClientPage = driver.find_element_by_xpath(xpatchRefClientsPage)
+refClientPage.click()
+print("Clicked add client")
+time.sleep(secondsToWaitLoadelementOnPage) # wait load form
+
+
+
 driver.quit()
 
 
-
-# Создать на нашем сайте пустой аккаунт, в качестве имени при регистрации нужно указать "Тестовое задание"
 # Добавить 50 клиентов с различными датами рождения с 01.01.1970 по 31.12.2000.
 # У клиентов должен быть указан код. У первого из клиентов - равен 1, у второго - 2, у последующих - сумме кодов двух предыдущих клиентов.
 # Клиентам, которым сейчас > 30 лет, предоставляется скидка на все услуги 7%; клиентам, которым сейчас от 20 до 30 лет, предоставляется скидка на все услуги 5%; клиентам, которым сейчас до 20 лет, предоставляется скидка на все услуги 3%
